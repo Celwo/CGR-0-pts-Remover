e30e8f63f4ee12602e2091481a9c231275d724bf
Celwo <83187182+Celwo@users.noreply.github.com>
2022-03-13 20:54:44 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from os import path, listdir, makedirs, remove
+
+bad_words = ['Balance = 0', 'Solde : 0', 'Points = 0.0Points']
+
+if not path.exists("Removed"):
+        makedirs("Removed")
+with open('hits.txt') as oldfile, open('Removed/CGR.txt', 'w') as newfile:
+    for line in oldfile:
+        if not any(bad_word in line for bad_word in bad_words):
+            newfile.write(line)
+print(f"""
+______                                  _   _ 
+| ___ \                                | | | |
+| |_/ /___ _ __ ___   _____   _____  __| | | |
+|    // _ \ '_ ` _ \ / _ \ \ / / _ \/ _` | | |
+| |\ \  __/ | | | | | (_) \ V /  __/ (_| | |_|
+\_| \_\___|_| |_| |_|\___/ \_/ \___|\__,_| (_)
+                                              
+""")
